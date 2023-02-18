d9367a579514c1855ffb85990fc619b114a584b9
brettmostert <brettmostert@gmail.com>
2023-02-18 11:24:09 +0200

    chore(repo): yolo

---
+++ apps/clippy/src/main.rs
@@ -0,0 +1,8 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+fn main() {
+    let my_list = ["One", "Two", "Three"];
+    for i in my_list {
+        println!("{i} ss");
+    }
+}
--- clippy/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![deny(clippy::all)]
-#![warn(clippy::pedantic)]
-fn main() {
-   let my_list = [ "One", "Two", "Three" ];
-   for i in my_list {
-      println!("{i} ss");
-   }
-}
